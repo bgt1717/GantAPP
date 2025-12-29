5b19ef95045ff707d910848c8208080d83b2fd2d
bgt1717 <brycetown10@gmail.com>
2025-12-28 18:49:21 -0600

    models set up

--- server/models/Project.js
+++ server/models/Project.js
@@ -2,8 +2,22 @@ import mongoose from "mongoose";
 
 const projectSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   { timestamps: true }
 );
